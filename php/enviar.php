2792241f6234f9b2be7afd2025a19500a2748970
Wilbort Encomenderos <wilbort_enru@hotmail.com>
2017-03-27 16:41:16 -0500

    update-270317
    update-270317

--- php/enviar.php
+++ php/enviar.php
@@ -1,8 +1,10 @@
 <?php
 $fecha=date("d-m-y");
 $hora=date("h:i:s");
-$destino="whuaman@afaingenieros.com";
-$destino2="wencomenderos@afaingenieros.com";
+$destino="grupoafa@afaingenieros.com";
+$destino2="ccueva@afaingenieros.com";
+$destino3="omundaca@afaingenieros.com";
+$destino4="wencomenderos@itm.edu.pe";
 $asunto="Consulta web";
 $desde= 'form:' .$_POST['email'] ;
 
@@ -16,5 +18,7 @@ Enviado el $fecha a las $hora\n";
 
 mail($destino,$asunto,$comentario,$desde);
 mail($destino2,$asunto,$comentario,$desde);
+mail($destino3,$asunto,$comentario,$desde);
+mail($destino4,$asunto,$comentario,$desde);
 header("location: ../contacto.html");
 ?>
